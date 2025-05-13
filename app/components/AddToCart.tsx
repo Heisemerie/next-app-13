6f4c1e7b28de3e5038e8175e8b313212a0538ca9
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-13 17:25:17 +0100

    Fundamentals: client and server components

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return <button onClick={() => console.log("Clicked")}>Add to Cart</button>;
+};
+
+export default AddToCart;
