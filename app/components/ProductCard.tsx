f35402446dbdddf9270720e263ab857c3280949f
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-13 23:07:33 +0100

    Styling: tailwind css

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -4,10 +4,16 @@ import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
-      {/* in the line nextjs uses PostCSS to generate unique classnames to prevent clashes (is add CSS scope) */}
-      <AddToCart />
-    </div>
+    <>
+      <div className={styles.card}>
+        {/* in the line nextjs uses PostCSS to generate unique classnames to prevent clashes (is add CSS scope) */}
+        <AddToCart />
+      </div>
+      <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
+        {/* styled with tailwind */}
+        <AddToCart />
+      </div>
+    </>
   );
 };
 
