ad79afc006ab6c7a1067161bfe3b0fb289e251a3
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-13 21:24:48 +0100

    Styling: css modules

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AddToCart from "./AddToCart";
+import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className={styles.card}>
+      {/* in the line nextjs uses PostCSS to generate unique classnames to prevent clashes (is add CSS scope) */}
       <AddToCart />
     </div>
   );
