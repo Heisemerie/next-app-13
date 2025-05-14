ee522d399cc3633dcfc4ed125a111af30dabfb5e
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-14 07:19:10 +0100

    Routing and Navigation: introduction

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UserTable = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 }, //caching only works with the fetch() function and not third party libraries
+  });
+  const users: User[] = await res.json();
+  return (
+    <table className="table table-border">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,38 +1,13 @@
 import React from "react";
+import UserTable from "./UserTable";
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
-
-const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 10 }, //caching only works with the fetch() function and not third party libraries
-  });
-  const users: User[] = await res.json();
-
+const UsersPage = () => {
   return (
     <>
       <h1>Users</h1>
       {/* the line below refreshes in development but not in production */}
       <p>{new Date().toLocaleTimeString()}</p>
-      <table className="table table-border">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   );
 };
