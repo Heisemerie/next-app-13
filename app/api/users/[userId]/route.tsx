7f30376c9828fa2e644062fe3aad400eaaba4873
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-15 13:49:34 +0100

    Building APIs: creating an object

--- app/api/users/[userId]/route.tsx
+++ app/api/users/[userId]/route.tsx
@@ -12,3 +12,22 @@ export function GET(request: NextRequest, { params: { userId } }: Props) {
     return NextResponse.json({ error: "user not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "James" });
 }
+
+//PUT for replacing an object
+//PATCH for updating one or more properties
+export async function PUT(request: NextRequest, { params: { userId } }: Props) {
+  const body = await request.json();
+  // Validate the request body
+  // If invalid, return 400
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  // Fetch the user with the given id
+  // If user doesn't exist, return 404
+  if (userId > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  // Else, update user
+  // Return updated user
+  return NextResponse.json({ id: 1, name: body.name });
+}
