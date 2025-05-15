bff07236f7374ccf3fb664dda2fdba5389ebca3e
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-15 08:55:49 +0100

    Building APIs: creating an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -12,3 +12,13 @@ export function GET(request: NextRequest) {
     { id: 2, name: "James" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // Validate
+  // If invalid, return 400
+  // Else return data
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 }); //test with postman
+}
