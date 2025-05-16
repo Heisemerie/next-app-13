5945c56663700518b5e82fc225985d2381d4cc97
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-16 18:15:13 +0100

    Uploading files: showing uploaded images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,35 @@
 "use client";
-import { CldUploadWidget } from "next-cloudinary";
-import React from "react";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState(""); //to store the public id to be passed to the cloudinary image component
+
   return (
-    <CldUploadWidget uploadPreset="guxzjmiv">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt="An image" />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="guxzjmiv"
+        onSuccess={(result, widget) => {
+          console.log(result); // has a public id property in the info object
+          if (result.event !== "success") return;
+          const info = result.info as unknown as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
