d50f5565a716427bdebc42b969acf2ad546c2b40
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-17 03:06:52 +0100

    Authentication: protecting routes (middleware demo)

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  //this function gets executed on every request
+  //here we have the chance check the user session and redirect them to the login page
+  //we won't implement the logic here because it is already implemented in NextAuth
+  //this is for demonstration. we just show how middleware in Next.js works
+  return NextResponse.redirect(new URL("/new-page", request.url));
+}
+
+export const config = {
+  // *: zero or more params
+  // +: one or more params
+  // ?: zero or one params
+  matcher: ["/users/:id*"],
+}; //to specify specific paths for the middleware to run
