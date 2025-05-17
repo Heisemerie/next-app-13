575dcd00ba2cacaf200c81c7144be796dfe43455
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-17 11:47:48 +0100

    Optimizations: using fonts (google)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
 import AuthProvider from "./auth/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const roboto = Roboto({ subsets: ["latin"], weight: ['400','500'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="forest">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <AuthProvider>
           <NavBar />
           <main className="px-5">{children}</main>
