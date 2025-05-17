b35a1b991fbba4a75a3b864f7f61734cd521b270
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-17 07:12:11 +0100

    Sending Emails: creating an email template

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+interface Props {
+  name: string;
+}
+
+const WelcomeTemplate = ({ name }: Props) => {
+  return (
+    <Html>
+      <Preview>Welcome Aboard</Preview>
+      <Body>
+        <Container>
+          <Text>Hello {name}</Text>
+          <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default WelcomeTemplate;
